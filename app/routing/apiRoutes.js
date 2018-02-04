5db591c82f28dc87c5e5a77db723799d6f9b5a1b
Steve Marshall <stevesonoa@gmail.com>
2018-02-03 23:19:07 -0700

    Framework with initial routes setup.

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,11 @@
+module.exports = function(app, path) {
+	// Display all available friends
+	app.get("/api/friends", function(req, res) {
+
+	});
+
+	// Save the incoming result & determine the correct match
+	app.post("/api/friends", function(req, res) {
+
+	});
+}
